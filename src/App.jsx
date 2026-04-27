1bd54a3afff9978e5df55e798b6236ddcc88c7be
Qataadah <qaxiafridi@gmail.com>
2026-04-27 18:28:01 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-gray-700 h-screen w-full text-white '>
+      <h1>Hello, World!</h1>
+    </div>
+  )
+}
+
+export default App
